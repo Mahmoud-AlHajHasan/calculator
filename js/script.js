4ee9192a4308ab12eb48f7098cd7043905cf4db0
Mahmoud-AlHajHasan <alhajhasanmahmoud99@gmail.com>
2022-10-06 00:10:58 +0300

    Make a simple operate then equal calculator

--- js/script.js
+++ js/script.js
@@ -14,13 +14,63 @@ function divide(x, y) {
   return ((+x) / (+y))
 }
 
-function operate(x, y, z) {
-  return z(x, y)
+function operate(numOne, numTwo, operation) {
+  numOne = +x
+  numTwo = +displayValue
+  operation = operatorUsed
+  displayValue = operation(numOne, numTwo)
+  displayScreen.textContent = displayValue
+  numOne = displayValue
+
+  //re-add
+  operatorButtons.forEach(
+    button => {
+      button.addEventListener("click", storeNumber)
+      button.addEventListener("click", storeOperator)
+      button.addEventListener("click", switchWriteOver)
+    }
+  )
 }
 
 function populateDisplay() {
-  displayValue = + this.textContent
+  if (writeOver) {
+    displayValue = ''
+    writeOver = false
+  }
+  displayValue += this.textContent
+  displayScreen.textContent = displayValue
+}
+
+function storeNumber() {
   x = displayValue
+
+  //delete
+  operatorButtons.forEach(
+    button => {
+      button.removeEventListener("click", storeNumber)
+    }
+  )
+
+  return x
+}
+
+function storeOperator() {
+  operatorUsed = this.getAttribute("kind")
+  if (operatorUsed === "add") { operatorUsed = add }
+  if (operatorUsed === "subtract") { operatorUsed = subtract }
+  if (operatorUsed === "multiply") { operatorUsed = multiply }
+  if (operatorUsed === "divide") { operatorUsed = divide }
+
+}
+
+function switchWriteOver() {
+  writeOver = true
+  operatorButtons.forEach(
+    button => {
+      button.removeEventListener("click", switchWriteOver)
+    }
+  )
+
 }
 
 displayScreen = document.querySelector("#display > div")
@@ -33,10 +83,24 @@ const equalButton = document.querySelector(".equalSign")
 
 let displayValue = ''
 let operatorUsed = ''
+let x = ''
+let y = ''
+writeOver = false
 
+// first input
+digitButtons.forEach(
+  button => button.addEventListener("click", populateDisplay)
+)
+dotButton.addEventListener("click", populateDisplay, { once: true })
 
-// user input : click numbers and operators to operate
-// click more numbers and operators after last output
-// loop
+// operator first time
+operatorButtons.forEach(
+  button => {
+    button.addEventListener("click", storeNumber)
+    button.addEventListener("click", storeOperator)
+    button.addEventListener("click", switchWriteOver)
+  }
+)
 
-
+// first equal
+equalButton.addEventListener("click", operate)
