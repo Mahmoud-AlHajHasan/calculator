99954b9bcb415b4832a32d3faf218b8079bd87aa
Mahmoud-AlHajHasan <alhajhasanmahmoud99@gmail.com>
2022-10-05 18:26:10 +0300

    Make populateDisplay()

--- js/script.js
+++ js/script.js
@@ -1,20 +1,33 @@
 function add(x, y) {
-  return x+y
+  return x + y
 }
 
-function subtract(x,y) {
-  return x-y
+function subtract(x, y) {
+  return x - y
 }
 
-function multiply(x,y) {
-  return x*y
+function multiply(x, y) {
+  return x * y
 }
 
-function divide(x,y) {
-  return x/y
+function divide(x, y) {
+  return x / y
 }
 
-function operate(x,y,basicMathFunction) {
-  return basicMathFunction(x,y)
+function operate(x, y, basicMathFunction) {
+  return basicMathFunction(x, y)
 }
 
+let numBtns = document.querySelectorAll(".num")
+numBtns.forEach(numBtn => numBtn.addEventListener("click", populateDisplay))
+
+const numBtnDot = document.querySelector(".dot")
+
+numBtnDot.removeEventListener("click", populateDisplay)
+numBtnDot.addEventListener("click", populateDisplay, {once:true})
+
+let displayValue = 0;
+
+function populateDisplay(e) {
+  displayValue += e.target.textContent
+}
